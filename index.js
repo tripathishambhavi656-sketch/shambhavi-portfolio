24c7a7acce871e2b521550adf419fcc8bd28af73
Shambhavi Tripathi <tripathishambhavi656@gmail.com>
2026-03-27 23:11:52 +0530

    Add files via upload
    Initial portfolio upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Smooth scroll
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
